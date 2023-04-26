7f1ace825d5b6d653dd311d6c49535c933e0c8e8
Shawn Nectar <64110243+ShawnNectar@users.noreply.github.com>
2023-04-26 19:02:56 -0300

    organized - wait for screen (values) - turtle optimized

--- graph.py
+++ graph.py
@@ -1,49 +1,51 @@
-from turtle import *
+from turtle import Turtle, Screen
 import math
 
 
-g = Turtle()
-
-g.speed(0)
-
-height = 3000
-width = 3000
-screen = Screen()
-screen.screensize(width, height)
-
-g.pu()
-g.bk(3000)
-g.pd()
-g.fd(6000)
-g.pu()
-g.bk(3000)
-g.pd()
-g.lt(90)
-g.fd(3000)
-g.pu()
-g.bk(6000)
-g.pd()
-g.fd(3000)
+def get_input():
+    print('y = kx + b')
+    k = float(input('k: '))
+    b = float(input('b: '))
+    return k, b
 
 
-print('y = kx + b')
+def draw_graph(k, b):
+    g = Turtle()
+    g.speed(0)
 
-k = float(input('k: '))
+    height = 3000
+    width = 3000
+    screen = Screen()
+    screen.screensize(width, height)
 
-b = float(input('b: '))
+    g.penup()
+    g.back(3000)
+    g.pendown()
+    g.forward(6000)
+    g.penup()
+    g.back(3000)
+    g.pendown()
+    g.left(90)
+    g.forward(3000)
+    g.penup()
+    g.back(6000)
+    g.pendown()
+    g.forward(3000)
 
-g.forward(b * 20)
+    g.forward(b * 20)
+    g.color('blue')
 
-g.color('blue')
+    kf = math.degrees(math.atan(k))
+    g.right(kf)
+    g.forward(2000)
+    g.right(180)
+    g.forward(4000)
+    g.right(180)
+    g.forward(4000)
+    g.hideturtle()
 
-kf = math.degrees(math.atan(k))
-g.right(kf)
 
-
-g.fd(2000)
-g.rt(180)
-g.fd(4000)
-g.rt(180)
-g.fd(4000)
-
-input("Press any key to exit ...")
+if __name__ == '__main__':
+    k, b = get_input()
+    draw_graph(k, b)
+    input("Press any key to exit ...")
